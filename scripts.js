4667820a759437238a76c2553d2d8f77789e88e3
unknown <egwomevan323@gmail.com>
2024-01-29 09:51:47 +0100

    all changes made in the night

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ addDept.addEventListener('click',()=>{
 })
 cancelDept.addEventListener('click',(e)=>{
   e.preventDefault()
-  deptForm.style.top = '100vh'
+  deptForm.style.top = '1000vh'
   deptForm.style.left = 0
 })
 
@@ -29,7 +29,7 @@ addStaff.addEventListener('click',()=>{
 })
 cancelStaff.addEventListener('click',(e)=>{
   e.preventDefault()
-  staffForm.style.top = '100vh'
+  staffForm.style.top = '1000vh'
   staffForm.style.left = 0
 })
 
@@ -44,59 +44,20 @@ function openTab(evt, tabName) {
   }
 
   // Display the selected tab content and set 'active' class to the clicked tab link
-  document.getElementById(tabName).style.display = 'grid';
-  document.getElementById(tabName).classList.remove("d-none");
+  document.getElementById(tabName).style.display = 'grid'
+  document.getElementById(tabName).classList.remove("d-none")
 }
 
 // Get all elements with class=".nav-link" and attach a click event listener
-var tabLinks = document.querySelectorAll(".nav-link");
+var tabLinks = document.querySelectorAll(".nav-link")
 for (var i = 0; i < tabLinks.length; i++) {
   tabLinks[i].addEventListener("click", function(event) {
-    event.preventDefault();
-    var tabName = this.getAttribute("href").substring(1);
-    openTab(event, tabName);
-  });
+    event.preventDefault()
+    var tabName = this.getAttribute("href").substring(1)
+    openTab(event, tabName)
+  })
 }
 
-// staffContainer.addEventListener('click', function(event) {
-//   if (event.target.classList.contains('edit-btn')) {
-//     // Handle edit button click here
-//     const card = event.target.closest('.card');
-//     if (card) {
-//       editToggle()
-//       console.log('Edit clicked for card:', card);
-//     }
-//   }
-// });
-
-// Function to toggle between card and form on "Edit" button click
-const editToggle = ()=>{
-  var editBtns = document.querySelectorAll('.edit-btn');
-  for (let i = 0; i < editBtns.length; i++) {
-    editBtns[i].addEventListener('click', function() {
-      var cardBody = this.parentElement;
-      var card = cardBody.parentElement;
-      var editForm = cardBody.querySelector('.edit-form');
-      card.classList.add('card-flipped');
-      editForm.classList.remove('d-none');
-      // editForm.classList.add('d-block');
-    });
-  }
-}
-editToggle()
-// Function to cancel editing and flip back to the card
-const cancelToggle = ()=>{
-  var cancelBtns = document.querySelectorAll('.cancel-btn');
-  for (let j = 0; j < cancelBtns.length; j++) {
-    cancelBtns[j].addEventListener('click', function() {
-      var editForm = this.parentElement;
-      var cardBody = editForm.parentElement;
-      var card = cardBody.parentElement;
-      card.classList.remove('card-flipped');
-    });
-  }
-}
-cancelToggle()
 function open_sidebar() {
   document.getElementById("main").style.marginLeft = "35%";
   document.getElementById("side-bar").style.width = "30%";
@@ -133,29 +94,17 @@ async function fetchData() {
         // Append content to the card
         card.innerHTML = 
         `
-        <div class="card-body">
-        <div class="card-front">
-        <h5 class="card-title">${dept.name}</h5>
-        <h5 class="card-title">Staff: ${dept.staff_count == undefined ? '0' : dept.staff_count}</h5>
-        <button class="btn btn-primary edit-btn">Edit Department Name</button>
-        </div>
-        <div class="card-back">
-        <!-- Form for editing department name -->
-        <form class="edit-form">
-        <div class="mb-3">
-        <label for="departmentName" class="form-label">New Department Name</label>
-        <input type="text" class="form-control" id="departmentName">
-        </div>
-        <button type="submit" class="btn btn-success">Save</button>
-        <button type="button" class="btn btn-secondary cancel-btn">Cancel</button>
-        </form>
-        </div>
-        </div>  
+          <h5 >${dept.name}</h5>
+          <h5>Staff: ${dept.staff_count == undefined ? '0' : dept.staff_count}</h5>
+          <div class="card-buttons-div">
+            <button id=${dept._id} class="edit-dept-btn btn-edit">Edit</button>
+            <button id=${dept._id} class="btn-delete">
+              <img src="./img/bx-trash.png" />
+            </button>
+          </div>
         `
         // Append the newly created card to the container
         deptContainer.appendChild(card);
-        editToggle()
-        cancelToggle()
       })
 
       deptJson.data.forEach((dept)=>{
@@ -187,30 +136,55 @@ async function fetchData() {
         // Append content to the card
         card.innerHTML = 
         `
-        <div class="card-body">
-        <div class="card-front">
-        <h5 class="card-title">${staff.name} ${staff.surname}</h5>
-        <button class="btn btn-primary edit-btn">Edit Staff Name</button>
-        </div>
-        <div class="card-back">
-        <!-- Form for editing staff name -->
-        <form class="edit-form">
-        <div class="mb-3">
-        <label for="staffName" class="form-label">New Staff Name</label>
-        <input type="text" class="form-control" id="staffName">
-        </div>
-        <button type="submit" class="btn btn-success">Save</button>
-        <button type="button" class="btn btn-secondary cancel-btn">Cancel</button>
-        </form>
-        </div>
-        </div>
+          <h5 class="card-title">${staff.name} ${staff.surname}</h5>
+          <div class="card-buttons-div">
+            <button id=${staff._id} class="edit-staff-btn btn-edit">Edit</button>
+            <button id=${staff._id} class="btn-delete">
+              <img src="./img/bx-trash.png" />
+            </button>
+          </div>
         `
         // Append the newly created card to the container
         staffContainer.appendChild(card);
-        editToggle()
-        cancelToggle()
       })
     }
+
+    //this are the event listeners for the edit of the department section
+    const editDeptBtn = document.getElementsByClassName('edit-dept-btn')
+    const editDeptForm = document.getElementById('edit-dept-form')
+    const cancelEditDept = document.getElementById('cancel-edit-dept')
+
+    Array.from(editDeptBtn).forEach((dept)=>{
+      dept.addEventListener('click',()=>{
+        console.log('clicking')
+        editDeptForm.style.top = 0
+        editDeptForm.style.left = 0
+      })
+    })
+    cancelEditDept.addEventListener('click',(e)=>{
+      e.preventDefault()
+      editDeptForm.style.top = '1000vh'
+      editDeptForm.style.left = 0
+    })
+
+    //this are the event listeners for the edit of the staff section
+    const editStaff = document.getElementsByClassName('edit-staff-btn')
+    const editStaffForm = document.getElementById('edit-staff-form')
+    const cancelEditStaff = document.getElementById('cancel-edit-staff')
+
+    Array.from(editStaff).forEach((staff)=>{
+      staff.addEventListener('click',()=>{
+        editStaffForm.style.top = 0
+        editStaffForm.style.left = 0
+      })
+    })
+
+    cancelEditStaff.addEventListener('click',(e)=>{
+      e.preventDefault()
+      editStaffForm.style.top = '1000vh'
+      editStaffForm.style.left = 0
+    })
+
   } catch (error) {
     console.error('Fetch error:', error);
   }
